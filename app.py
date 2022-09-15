72e2bea1cc2e4de1faf3851dd1bc78c899d0531b
Johnathan Booy <johnny_music@icloud.com>
2022-09-15 08:40:06 -0700

    Displaying the board

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
+from flask import Flask, render_template
 from boggle import Boggle
 
 boggle_game = Boggle()
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def show_boggle_board():
+    board = boggle_game.make_board()
+    return render_template('boggle.html', board=board)
--- test.py
+++ test.py
@@ -4,6 +4,5 @@ from flask import session
 from boggle import Boggle
 
 
-class FlaskTests(TestCase):
-
-    # TODO -- write tests for every view function / feature!
+# class FlaskTests(TestCase):
+#     # TODO -- write tests for every view function / feature!
